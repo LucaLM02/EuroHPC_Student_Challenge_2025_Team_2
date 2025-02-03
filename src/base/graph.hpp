101ac4b7998f62f07fb605fa19fb84900efdd3f0
GiacomoPauletti <giacomo.pauletti@gmail.com>
2025-02-03 10:45:35 +0000

    src/base/dimacs_graph.cpp

--- src/base/graph.hpp
+++ src/base/graph.hpp
@@ -14,7 +14,7 @@
  *  The graph can be modified, since it is useful for applying Zykov coloring algorithm.
  *  Vertex and edges can be added and removed. 2 vertices can be merged (see MergeVertices 
  *  for a more detailed description)
-*/
+ */
 class Graph {
     public:
         /**
@@ -29,12 +29,15 @@ class Graph {
          *           that the edge wasn't already present. If this is not true,
          *           the edge will be duplicated, leading possibly to undesired
          *           behaviours
-        */
+         */
         virtual void AddEdge(int v, int w) = 0;
 
         /**
          *  @brief removes the edge <v,w>=<w,v> from the graph
         */
+        /**
+         *  @brief removes the edge <v,w>=<w,v> from the graph
+         */
         virtual void RemoveEdge(int v, int w) = 0;
         /**
          *  @brief adds a vertex to the graph
@@ -76,7 +79,6 @@ class Graph {
 
         /**
          *  @brief gets the neighbours of `vertex` as a vector
-
          *  @details
          *  Gets the neighbours of `vertex` as a vector <br>
          *  Child classes might impose a certain order in the result
@@ -98,27 +100,55 @@ class Graph {
         virtual bool HasEdge(int v, int w) const = 0;
 
         /**
-         *  @brief returns set of vertices used in the graphc
-
+         *  @brief returns set of vertices used in the graph but without a precise order
          *  @details
-         *  Returns set of vertices used in the graph <br>
+         *  Returns set of vertices used in the graph without a precise order<br>
          *  Note that vertices aren't necessarily the number between 0 and GetNumVertices(),
          *  they shall be any sequence of numbers >= 0
          */
-        virtual void GetVertices(std::set<int> &result) const = 0;
+        virtual void GetUnorderedVertices(std::set<int> &result) const = 0;
         /**
-         *  @brief returns vector of vertices used in the graph
-
+         *  @brief returns vector of vertices used in the graph. The order matters here
          *  @details
          *  Returns vector of vertices used in the graph <br>
          *  Note that vertices aren't necessarily the number between 0 and GetNumVertices(),
          *  they shall be any sequence of numbers >= 0
          */
-        virtual void GetVertices(std::vector<int> &result) const = 0;
+        virtual const std::vector<int>& GetVertices() const = 0;
+
+        /**
+         *  @brief sets the vertices of the graph. Used typically to set the order of them
+         *  @warning for efficiency reasons, this method does not delete the edges which do not have one of the 2 vertices after this method is called
+         */
+        virtual void SetVertices(std::vector<int>& vertices) = 0;
 
         virtual size_t GetNumVertices() const = 0;
         virtual size_t GetNumEdges() const = 0;
-
+        /**
+         * @brief Get the degree of `vertex`
+         * 
+         * @param vertex vertex of which the degree is returned
+         * @return unsigned int degree of `vertex`
+         */
+        virtual unsigned int GetDegree(int vertex) const = 0;
+        /**
+         * @brief Returns an array of degrees, one for each vertex
+         * Note: no order is imposed
+         * @return std::vector<int> array of degrees
+         */
+        virtual std::vector<int> GetDegrees() const = 0;
+        /**
+         * @brief Returns the maximum degree (delta(G)) among the degree of all vertices of this graph
+         * 
+         * @return unsigned int maximum degree of the graph (delta(G))
+         */
+        virtual unsigned int GetMaxDegree() const = 0;
+        /**
+         * @brief Gets the vertex with the max degree 
+         * 
+         * @return int the vertex with the max degree
+         */
+        virtual int GetVertexWithMaxDegree() const = 0;
 
 };
 
