c333fdb7f020f27d28ffc8b98a13260710036365
GiacomoPauletti <giacomo.pauletti@gmail.com>
2025-02-10 16:51:27 +0000

    Finished recolor

--- src/color/recolor.cpp
+++ src/color/recolor.cpp
@@ -1,6 +1,220 @@
 #include "recolor.hpp"
 
-unsigned int GreedySwapRecolorStrategy::Recolor(VertexSet& vertices, const Edges& edges, std::vector<unsigned short>& coloring) const {
+unsigned int GreedySwapRecolorStrategy::Recolor(Graph& graph) const 
+{
+    std::vector<unsigned short> coloring = graph.GetFullColoring();
+    SwapRecolorStructure data(graph, coloring, 50);
+    data.FillWithData();
+    int result = data.Recolor();
 
-    return 0;
+    graph.SetFullColoring(coloring);
+    return result;
+}
+
+VertexRecolorData::VertexRecolorData()
+    : _old_color{NOT_ASSIGNED},
+      _vertex{NOT_ASSIGNED}
+{
+    std::random_device dev;
+    _random_generator = std::make_unique<std::mt19937>(dev());
+}
+
+VertexRecolorData::VertexRecolorData(int vertex,
+                                     std::vector<unsigned short>* coloring,
+                                     unsigned short max_color)
+    : _vertex{vertex}, _old_color{NOT_ASSIGNED},
+      _coloring{coloring}, _max_color{max_color}
+{
+    std::random_device dev;
+    _random_generator = std::make_unique<std::mt19937>(dev());
+}
+
+inline void VertexRecolorData::AssignColor(unsigned short color)
+{
+    if ( _old_color == NOT_ASSIGNED ) {
+        _old_color = (*_coloring)[_vertex];
+    }
+    (*_coloring)[_vertex] = color;
+}
+
+SwapRecolorStructure::SwapRecolorStructure(Graph &graph, std::vector<unsigned short>& coloring, int threshold)
+: _graph{graph}, _vertex_to_data(_graph.GetHighestVertex()+1), 
+  _dont_color{false}, _threshold{threshold}, _coloring{coloring}
+{
+}
+
+void SwapRecolorStructure::FillWithData()
+{
+    _graph.SortByColor();
+    std::vector<int> vertices = _graph.GetVertices();
+    unsigned int max_color = _graph.GetColor(vertices[0]);
+
+
+    int counter = 0;
+    for (int vertex : vertices ) {
+        if ( _graph.GetColor(vertex) == max_color ) {
+            counter++;
+            if ( counter == _threshold ) {
+                _dont_color = true;
+                return;
+            }
+        } else {
+            break;
+        }
+    }
+
+    std::vector<int> neighbours;
+    std::vector<int> other_neighbours;
+    
+    /**
+     * @brief for each colors, true if a particular vertix can be recolored with it
+     *        false otherwise
+     * @note + 1 since first element has index 0 and is skipped
+     */
+    std::vector<bool> colors_availability(max_color + 1);   
+    /**
+     * @brief all colors with colors_availability[color] = true
+     */
+    std::vector<unsigned short> available_colors;
+    available_colors.reserve(max_color);
+
+    std::vector<unsigned short> full_coloring = _graph.GetFullColoring();
+
+    for ( int vertex : vertices ) {
+        if ( full_coloring[vertex] < max_color ) {
+            break;
+        }
+
+        // initilizing in _vertex_to_data each neighbour of the current vertex
+        _graph.GetNeighbours(vertex, neighbours);
+        // also initilizing the current vertex data structure
+        //neighbours.push_back(vertex);
+        for ( int neighbour : neighbours ) {
+
+            VertexRecolorData& data = _vertex_to_data[neighbour];
+            // if is assigned then this vertex was already visited and added
+            if ( data.GetVertex() == VertexRecolorData::NOT_ASSIGNED ) 
+            {
+                _graph.GetNeighbours(neighbour, other_neighbours);
+                data.InitColoring(&_coloring, max_color);
+                data.InitVertex(neighbour, other_neighbours);
+            }
+        }
+    }
+}
+
+bool SwapRecolorStructure::Recolor()
+{
+    if ( _dont_color ) {
+        return false;
+    }
+
+    std::vector<int> vertices = _graph.GetVertices();
+    unsigned int max_color = _graph.GetColor(vertices[0]);
+
+    // finds the first vertex with color < max_color
+    int index;
+    for (index = 0; 
+         index < vertices.size() && _graph.GetColor(vertices[index]) == max_color; 
+         index++)
+    {
+    }
+
+    // erases all vertices with non maximum color
+    vertices.erase(vertices.begin()+index, vertices.end());
+
+    return this->RecolorBody(vertices);
+}
+
+bool SwapRecolorStructure::RecolorBody(std::vector<int> &vertices)
+{
+    // recursion basic case
+    if ( vertices.size() == 0 ) {
+        return true;
+    }
+
+    unsigned short max_color = _graph.GetColor(vertices[0]);
+    // removes the current vertex
+    int current_vertex = vertices[vertices.size()-1];
+    vertices.pop_back();
+
+    std::vector<int> neighbours;
+    _graph.GetNeighbours(current_vertex, neighbours);
+    std::vector<bool> has_been_recolored(neighbours.size());
+
+    bool successfully_recolored;
+
+    VertexRecolorData* current_neighbour_data;
+
+    /* Handled cases:
+        1) v1 <----> neighb <----> v2
+        
+        2)     v1              v2
+               |               |
+           neighb_1         neighb_2
+
+        3)     v1              v2
+               |               |
+           neighb_1 <----> neighb_2            
+    */
+
+    // for each color, tries to assign it to the current vertex and change the neighbours with that color.
+    // The neighbours with a different color cannot be colored to the current color until the current vertex
+    // changes it's color.
+    // Then, when a valid coloring is found, starts coloring the next vertex (recursive call)
+    for (unsigned short color=1; color < max_color; color++ ) {
+
+        _coloring[current_vertex] = color;
+        successfully_recolored = true;
+
+        std::fill(has_been_recolored.begin(), has_been_recolored.end(), false);
+
+        for (int neighbour_index = 0; neighbour_index < neighbours.size(); neighbour_index++ ) {
+            current_neighbour_data = &(_vertex_to_data[neighbours[neighbour_index]]);
+
+
+            if ( current_neighbour_data->GetCurrentColor() == color ) {
+                // if it has the same color, then change it
+
+                has_been_recolored[neighbour_index] = true;
+
+                // if unable to recolor, reverts the actions
+                if ( !current_neighbour_data->Recolor() ) {
+                    for ( int i=0; i < neighbour_index; i++ ) {
+                        if ( has_been_recolored[i] ) {
+                            // reverts color to what was previously
+                            _vertex_to_data[neighbours[i]].AssignColor(color);
+                        } 
+                    }
+                    // going to the next color
+                    successfully_recolored = false;
+                    break;
+                }
+            } 
+        }
+
+        // going to the next color
+        if ( !successfully_recolored ) {
+            _coloring[current_vertex] = max_color;
+            continue;
+        }
+
+        // recolors also the next vertices
+        if ( this->RecolorBody(vertices) ) {
+            vertices.push_back(current_vertex);
+            return true;
+        } else {
+            // reverting all the actions done during this for-each color iteration
+
+            for (int neighbour_index = 0; neighbour_index < neighbours.size(); neighbour_index++ ) {
+                if ( has_been_recolored[neighbour_index] ) {
+                    // reverts color to what was previously
+                    _vertex_to_data[neighbours[neighbour_index]].AssignColor(color);
+                } 
+            }
+        }
+    }
+
+    vertices.push_back(current_vertex);
+    return false;
 }
--- src/color/recolor.hpp
+++ src/color/recolor.hpp
@@ -1,54 +1,210 @@
 #ifndef RECOLOR_HPP
 #define RECOLOR_HPP
 
-#include "common.hpp"
+#include "graph.hpp"
+#include <random>
 
-/*
-    @brief abstract functional class that wraps Recolor method; (partially) recolors a graph 
-           to reduce the maximum color number
-    See the method description
-*/
+/**
+ *  @brief abstract functional class that wraps Recolor method; (partially) recolors a graph 
+ *         to reduce the maximum color number
+ *  @see the method description
+ */
 class RecolorStrategy {
     public:
-        /*
-            @brief tries to reduce the maximum color number of a certain coloring
-
-            @param vertices     set of vertices which compose the graph
-            @param edges        set of edges which compose the graph
-            @param coloring     full coloring of the graph
-
-            @returns            0 if recoloring failed, otherwise how much the highest color was reduced
-        */
-        virtual unsigned int Recolor(VertexSet& vertices, const Edges& edges, 
-                                    std::vector<unsigned short>& coloring) const = 0;
+        /**
+         *  @brief tries to reduce the maximum color number of a certain coloring by 1
+         * 
+         *  @returns 0 if recoloring failed, otherwise how much the highest color was reduced
+         */
+        virtual unsigned int Recolor(Graph& graph) const = 0;
 };
 
-/*
-    @brief functional class for the Recolor method; (partially) recolors a graph to reduce the maximum color number
-    See the method description
-*/
+/**
+ *  @brief functional class for the Recolor method; (partially) recolors a graph to reduce the maximum color number
+ *  @see the method description
+ */
 class GreedySwapRecolorStrategy : public RecolorStrategy {
     public:
-        /*
-            @brief tries to reduce the maximum color number of a certain coloring by greedly 
-                swapping colors
+        /**
+         *  @brief tries to reduce the maximum color number by 1 of a certain coloring by greedly 
+         *         swapping colors
+         * 
+         *  @details For each vertex with the highest degree, reduces the color by swapping 
+         *           it with a neighbour, which will be given another color. 
+         * 
+         * 
+         *  @returns            0 if recoloring failed, otherwise how much the highest color was reduced
+         */
+        unsigned int Recolor(Graph& graph) const override;
+};
 
-            @details For each vertex with the highest degree, reduces the color by swapping 
-                     it with a neighbour, which will be given another color. 
-                     When all the vertices with the highest degree are reduced to a lower degree,
-                     a local variable, `reduction_counter`, is incremented by one and the algorithm
-                     proceeds with the new highest degree.
-                     If any of this passages is not feasible, then algorithm stops and the current 
-                     value of `reduction_counter` is returned
+/**
+ * @brief contains all the data needed about a neigbhour of a vertex which 
+ *        GreedySwapRecolorStrategy want to reduce the color
+ */
+class VertexRecolorData {
+    public:
+        const static unsigned short NOT_ASSIGNED = -1;
 
-            @param vertices     set of vertices which compose the graph
-            @param edges        set of edges which compose the graph
-            @param coloring     full coloring of the graph
+        VertexRecolorData();
+        VertexRecolorData(int vertex,
+                          std::vector<unsigned short>* coloring,
+                          unsigned short max_color);
 
-            @returns            0 if recoloring failed, otherwise how much the highest color was reduced
+        /**
+         * @brief initializes the vertex to which this data is assigned
+         * 
+         * @param current_color 
+         */
+        inline void InitVertex(int vertex, std::vector<int> neighbours) {
+            _vertex = vertex;
+            _neighbours = neighbours;
+        }
+
+        inline int GetVertex() {
+            return _vertex;
+        }
+
+        /**
+         * @brief gives to this class the global array in which the coloring of the graph
+         *        is stored
+         * 
+         * @param colors 
+         */
+        inline void InitColoring(std::vector<unsigned short>* coloring, unsigned short max_color) {
+            _coloring = coloring;
+            _max_color = max_color;
+        }
+
+        inline unsigned short GetCurrentColor() {
+            return (*_coloring)[_vertex];
+        }
+
+        /**
+         * @brief reverts the color to the old one
+         * 
+         */
+        inline void RevertColor() {
+            if ( _old_color != NOT_ASSIGNED ) {
+                (*_coloring)[_vertex] = _old_color;
+                _old_color = NOT_ASSIGNED;
+            }
+        }
+
+        /**
+         * @brief whether to this vertex can be assigned a new color or not.
+         * 
+         * @return true 
+         * @return false 
+         */
+        inline bool IsRecolorable() {  
+            std::vector<bool> color_availability(_max_color+1, false);
+            for ( int neighbour : _neighbours ) {
+                color_availability[(*_coloring)[neighbour]] = false;
+
+            }
+            
+            std::vector<int> available_colors;
+            available_colors.reserve(_max_color+1);
+            for (int i = 1; i < _max_color+1; i++) {
+                if ( color_availability[i] ) {
+                    available_colors.push_back(i);
+                }
+            }
+
+            if ( color_availability.size() == 0 ) {
+                return false;
+            }
+            return true;
+        }
+
+        /** 
+         * @brief assigns a new color from the available ones.
+         *        If the color is not the desired one, please do RevertColor() and
+         *        then again thism method
         */
-        unsigned int Recolor(VertexSet& vertices, const Edges& edges, 
-                            std::vector<unsigned short>& coloring) const override;
+        inline bool Recolor() {
+
+            if ( _neighbours.size() == 0 ) {
+                return true;
+            }
+
+            // retrieves the available colors
+            std::vector<bool> color_availability(_max_color, true);
+            color_availability[this->GetCurrentColor()] = false;
+            for ( int neighbour : _neighbours ) {
+                color_availability[(*_coloring)[neighbour]] = false;
+
+            }
+            
+            std::vector<int> available_colors;
+            available_colors.reserve(_max_color);
+            for (int i = 1; i < _max_color; i++) {
+                if ( color_availability[i] ) {
+                    available_colors.push_back(i);
+                }
+            }
+
+            // not colorable
+            if ( available_colors.size() == 0 ) {
+                return false;
+            } else if ( available_colors.size() == 1 ) {
+                if ( _old_color == NOT_ASSIGNED ) {
+                    _old_color = (*_coloring)[_vertex];
+                }
+                (*_coloring)[_vertex] = available_colors[0];
+                return true;
+            }
+
+
+            if ( _old_color == NOT_ASSIGNED ) {
+                _old_color = (*_coloring)[_vertex];
+            }
+
+            // randomly choosing a new color which is not forbidden
+            std::uniform_int_distribution<int> u(0, available_colors.size()-1);
+            (*_coloring)[_vertex] = available_colors[u(*_random_generator)];
+
+            return true;
+        }
+        inline void AssignColor(unsigned short color);
+        
+    protected:
+        int _vertex;
+        /**
+         * @brief previously assigned color, if any, otherwise NOT_ASSIGNED
+         * 
+         */
+        unsigned short _old_color;
+        /**
+         * @brief available colors to swap with
+         */
+        unsigned short _max_color;
+        std::vector<unsigned short>* _coloring;
+        std::vector<int> _neighbours;
+        std::unique_ptr<std::mt19937> _random_generator;
+};
+
+class SwapRecolorStructure {
+    public:
+        SwapRecolorStructure(Graph& graph, std::vector<unsigned short>& coloring, int threshold = 10);
+        void FillWithData();
+
+        bool Recolor();
+
+    private:
+        Graph& _graph;
+        std::vector<VertexRecolorData> _vertex_to_data;
+        std::set<int> _recolored_neighbours;
+        std::vector<unsigned short>& _coloring;
+        /**
+         * @brief if the number of vertices to recolor (aka with maximum color) is
+         *        greater or equal than threshold, do not color
+         */
+        int _threshold;
+        bool _dont_color;
+
+        bool RecolorBody(std::vector<int>& vertices);
 };
 
 #endif // RECOLOR_HPP
